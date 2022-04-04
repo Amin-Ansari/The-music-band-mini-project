bbd6df47eafb97bd06e05b31402dba2e6f4c7279
Amin Ansari <devansari@yahoo.com>
2022-04-04 19:34:25 +0430

    It's done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // The elements
 const theMenu = document.querySelector("#mob-menu");
 const theBarButton = document.querySelector("#bar-btn");
+const remainTicket = document.querySelector("#re-ticket");
+const buyButton = document.getElementsByTagName("button");
+let theNewNum = Number(remainTicket.innerHTML);
 
 // Function and methods
 function showAndHide() {
@@ -13,7 +16,22 @@ function outerClick(event) {
     }
   }
 }
+function newvalue() {
+  remainTicket.innerHTML = theNewNum;
+}
+function buyTicket() {
+  if (theNewNum > 0) {
+    theNewNum--;
+    newvalue();
+    window.alert("بلیط شما رزوشد");
+  } else if (theNewNum <= 0) {
+    window.alert("به دلیل اتمام بلیط، امکان رزرو بلیط برای شما وجود ندارد.");
+  }
+}
 
 // Envoking the functions and methods
 theBarButton.addEventListener("click", showAndHide);
 document.addEventListener("click", outerClick);
+buyButton[0].addEventListener("click", buyTicket);
+buyButton[1].addEventListener("click", buyTicket);
+buyButton[2].addEventListener("click", buyTicket);
